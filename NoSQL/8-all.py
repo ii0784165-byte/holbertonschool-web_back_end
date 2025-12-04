f50b5d500ec00e70419959291d36370146753455
ii0784165-byte <ii0784165@gmail.com>
2025-12-04 15:36:04 +0300

    task 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" 8-all.py """
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection
+
+    Args:
+        mongo_collection: pymongo collection object
+
+    Returns:
+        list of documents (empty list if none)
+    """
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
